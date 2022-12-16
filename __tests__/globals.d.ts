7216e23cd304b4a4eb03ff5d723b5b604946c880
Alexander Chaschin <ac@happynumbers.com>
2022-12-16 14:56:22 +0300

    add interface extension for matchers

---
+++ __tests__/globals.d.ts
@@ -0,0 +1,18 @@
+declare module '@jest/expect' {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  interface Matchers<R extends void | Promise<void>> {
+    toBeDisabled(): R;
+    toBeEmptyElement(): R;
+    toBeEnabled(): R;
+    toBeVisible(): R;
+    toContainElement(element: ReactTestInstance | null): R;
+    toHaveTextContent(text: string | RegExp, options?: { normalizeWhitespace: boolean }): R;
+    toHaveProp(attr: string, value?: unknown): R;
+    toHaveStyle(style: StyleProp<ViewStyle | TextStyle | ImageStyle>): R;
+    toHaveAccessibilityState(state: AccessibilityState): R;
+    toHaveAccessibilityValue(state: AccessibilityValueMatcher): R;
+
+    /** @deprecated This function has been renamed to `toBeEmptyElement`. */
+    toBeEmpty(): R;
+  }
+}
